1f05c56acebfd5da8642a425e62484dada4a3a0b
Varun Kumar <varunkumarsunkaraboina@gmail.com>
2024-08-31 09:06:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from '../src/Components/Navbar.jsx';
+import Hero from '../src/Components/Hero.jsx'
+import Analytics from '../src/Components/Analytics.jsx'
+import Newletter from '../src/Components/Newletter.jsx'
+import Card from '../src/Components/Card.jsx'
+import Footer from '../src/Components/Footer.jsx'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Hero/>
+      <Analytics/>
+      <Newletter/>
+      <Card/>
+      <Footer/>
     </div>
   );
 }
